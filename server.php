a1b7adf4e090269e6014985b65afe99b706044ce
pagaric <pagaric@cwa12.com>
2021-11-20 11:48:29 +0100

    modif server.php

--- server.php
+++ server.php
@@ -1,11 +1,18 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+/*
+Commande pour lancer le serveur interne de PHP
+en utilisant server.php pour simuler le fonctionnement
+du fichier .htaccess
+php -S localhost:8000 -t public server.php
+*/
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if($uri != '/' && file_exists(__DIR__ . '/public' .$uri)) {
+if($url != '/' && file_exists(__DIR__ . '/public' .$url)) {
     return false;
 }
 
 $_SERVER['SCRIPT_NAME'] = "/index.php";
 
 require_once __DIR__ .'/public/index.php';
+
